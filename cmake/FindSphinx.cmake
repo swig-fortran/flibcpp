4ae286246fdb5510fffeb852cb352c12b0ba821e
Seth R Johnson <johnsonsr@ornl.gov>
2019-09-10 17:54:11 -0400

    Fix documentation build

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -19,8 +19,3 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Sphinx
                                   "Failed to locate sphinx-build executable"
                                   SPHINX_EXECUTABLE)
-
-# Provide options for controlling different types of output
-option(SPHINX_OUTPUT_HTML "Output standalone HTML files" ON)
-option(SPHINX_OUTPUT_MAN "Output man pages" ON)
-
