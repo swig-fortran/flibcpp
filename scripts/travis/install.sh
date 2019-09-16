3276c2a3848144a1ae3423d204e577e19e0123cb
Seth R Johnson <johnsonsr@ornl.gov>
2019-09-16 12:25:18 -0400

    More fixes

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -5,16 +5,25 @@
 # Install dependencies. 
 ###############################################################################
 
-export FC=${FC:-gfortran}
+set -e
+
+mkdir -p ${INSTALL_ROOT}/bin
+
+if [ "${GENERATOR}" = "ninja" ]; then
+  # Install Ninja
+  pushd $(mktemp -d)
+  NINJA_VERSION=1.9.0.g99df1.kitware.dyndep-1.jobserver-1
+  curl -L https://github.com/Kitware/ninja/releases/download/v${NINJA_VERSION}/ninja-${NINJA_VERSION}_x86_64-linux-gnu.tar.gz \
+    | tar -xz --strip 1
+  mv ninja ${INSTALL_ROOT}/bin
+  popd
+  echo "Installed Ninja version: $(ninja --version | head -1)"
+fi
+  
 echo "Fortran compiler: ${FC}"
 if [ -n "${FC}" ]; then
   echo "Compiler version: $(${FC} | head -1)"
 fi
-
-INSTALL_ROOT=${PWD}/install
-mkdir -p ${INSTALL_ROOT}/bin
-export CMAKE_PREFIX_PATH=${INSTALL_ROOT}:${CMAKE_PREFIX_PATH}
-export PATH=${INSTALL_ROOT}/bin:${PATH}
 
 if [ "${FLIBCPP_DEV}" = "ON" ]; then
   # Install SWIG-fortran
@@ -30,17 +39,6 @@ if [ "${FLIBCPP_DEV}" = "ON" ]; then
 fi
 
 
-if [ "${GENERATOR}" = "ninja" ]; then
-  # Install Ninja
-  pushd $(mktemp -d)
-  NINJA_VERSION=1.9.0.g99df1.kitware.dyndep-1.jobserver-1
-  curl -L https://github.com/Kitware/ninja/releases/download/v${NINJA_VERSION}/ninja-${NINJA_VERSION}_x86_64-linux-gnu.tar.gz \
-    | tar -x --strip 1
-  mv ninja ${INSTALL_ROOT}/bin
-  popd
-  echo "Installed Ninja version: $(ninja --version | head -1)"
-fi
-  
 ###############################################################################
 # end of scripts/travis/install.sh
 ###############################################################################
