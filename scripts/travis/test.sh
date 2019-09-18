07cf608fd2be6ea6dcef7109b1719f6fbd5c659e
Seth R Johnson <johnsonsr@ornl.gov>
2019-09-18 13:38:18 -0400

    Enable valgrind tests in Travis

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -3,7 +3,12 @@
 # File  : scripts/travis/test.sh
 ###############################################################################
 
-cd ${BUILD_ROOT} && ctest --output-on-failure
+cd ${BUILD_ROOT}
+
+ctest --output-on-failure
+if [ "${FLIBCPP_DEV}" = "ON" ]; then
+  ctest -E examples -D ExperimentalMemCheck --output-on-failure
+fi
 
 ###############################################################################
 # end of scripts/travis/test.sh
