8911e428da2983b4efdebcb3fe94b9653e493f8d
Seth R Johnson <johnsonsr@ornl.gov>
2020-02-21 12:25:49 -0500

    Update FindSWIG for not being in official cmake modules

--- cmake/backport-cmake-318/FindSWIG.cmake
+++ cmake/backport-cmake-318/FindSWIG.cmake
@@ -101,7 +101,7 @@ if(SWIG_EXECUTABLE)
   endif()
 endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   SWIG HANDLE_COMPONENTS
   REQUIRED_VARS SWIG_EXECUTABLE SWIG_DIR
