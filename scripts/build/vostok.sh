58218296e5b0cb8771af9da0cf7e6234efaf898d
Seth R Johnson <johnsonsr@ornl.gov>
2020-08-22 14:07:36 -0400

    Tweak build script

--- scripts/build/vostok.sh
+++ scripts/build/vostok.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-SOURCE=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
+SOURCE=$(cd $(dirname "${BASH_SOURCE[0]}")/../.. && pwd)
 BUILD=${SOURCE}/build
 PREFIX=${SOURCE}/install
 
@@ -12,11 +12,13 @@ ${CMAKE} --version
 ${CMAKE} \
   -G Ninja \
   -D FLIBCPP_DEV=ON \
-  -D SWIG_EXECUTABLE="/rnsdhpc/code/build/swig-debug/swig"  \
-  -D SWIG_DIR="/rnsdhpc/code/src/swig/Lib" \
+  -D SWIG_EXECUTABLE="/rnsdhpc/code/_build/swig-debug/swig"  \
+  -D SWIG_DIR="/rnsdhpc/code/swig/Lib" \
   -D BUILD_SHARED_LIBS=ON \
   -D CMAKE_Fortran_FLAGS="-Wall -Wextra -Wimplicit-procedure -Wimplicit-interface -Wno-compare-reals" \
-  -D CMAKE_CXX_FLAGS="-Wall -Wextra" \
+  -D CMAKE_CXX_FLAGS="-Wall -Wextra -pedantic" \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   ${SOURCE}
-ninja
+ninja -v
+ctest --output-on-failure
+ninja install
