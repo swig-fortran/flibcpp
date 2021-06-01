d76cbd9928c0c88f5a6ef449fb11cfdb583879cc
Seth R. Johnson <johnsonsr@ornl.gov>
2021-05-31 20:03:53 -0400

    Shared cmake (#30)
    * Move cmake swig utilities into a separate cmake file
    * Fix installed config file
    * Upgrade CI to use focal and GCC9
    * Allow external installation of examples

--- scripts/build/vostok.sh
+++ scripts/build/vostok.sh
@@ -11,11 +11,12 @@ ${CMAKE} --version
 
 # NOTE: gcc 10.2 incorrectly warns about logical conversions, see
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=96319
-module load swig/4.0.2-fortran gcc/10
+module load swig/4.0.2-fortran gcc/11
 
 ${CMAKE} \
   -G Ninja \
   -D FLIBCPP_DEV=ON \
+  -D FLIBCPP_Fortran_STANDARD=03 \
   -D BUILD_SHARED_LIBS=ON \
   -D CMAKE_Fortran_FLAGS="-Wall -Wextra -pedantic -Wimplicit-procedure -Wimplicit-interface -Wno-compare-reals" \
   -D CMAKE_CXX_FLAGS="-Wall -Wextra -pedantic -Werror" \
--- scripts/travis/configure.sh
+++ scripts/travis/configure.sh
@@ -16,11 +16,11 @@ CXX_FLAGS="-Wall -Wextra -Werror"
 Fortran_FLAGS="-Wall -Wextra -Wimplicit-procedure -Wimplicit-interface -Wno-compare-reals -Wno-maybe-uninitialized -Werror"
 
 set -x
-cd ${BUILD_ROOT} && cmake -G "${CMAKE_GENERATOR}" \
+cd ${BUILD_ROOT} && ${CMAKE} -G "${CMAKE_GENERATOR}" \
   -D FLIBCPP_DEV=${FLIBCPP_DEV} \
   -D FLIBCPP_BUILD_EXAMPLES=ON \
   -D FLIBCPP_BUILD_TESTS=ON \
-  -D FLIBCPP_FORTRAN_STD="${FLIBCPP_FORTRAN_STD}" \
+  -D FLIBCPP_Fortran_STANDARD="${FLIBCPP_FSTD}" \
   -D CMAKE_CXX_FLAGS="${CXX_FLAGS}" \
   -D CMAKE_Fortran_FLAGS="${Fortran_FLAGS}" \
   -D CMAKE_INSTALL_PREFIX="${INSTALL_ROOT}" \
